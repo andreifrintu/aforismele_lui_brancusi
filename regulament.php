54b1924a079abf54be19d26a3b9a9a2986d17d03
glue021 <78766047+glue021@users.noreply.github.com>
2023-12-22 15:33:42 +0200

    Update navbar.php

--- regulament.php
+++ regulament.php
@@ -27,7 +27,79 @@
     <!-- includem componenta de navbar --->
     <?php include_once('static/pages/navbar.php'); ?>
 
-
+    <div class="invisible p-3">.</div>
+    
+    <h1 class="text-galben text-center fw-bold fs-1 text-uppercase"> Regulament </h1>
+    <div class="text-justify fs-5 w-md-100 m-auto">
+        <h1 class="fs-1"><a id="argument" href="#argument" class="text-galben text-underline">#Grupul țintă</a></h1>
+        <span class="fs-1 text-albastru">C</span>oncursul se adresează elevilor din învățământul gimnazial (clasele a VII-a/a VIII-a) și liceal, care au aptitudini artistice, literare şi preocupări culturale. Simpozionul are şi o secţiune pentru profesori. Condiţiile de participare se regăsesc mai jos în regulament. 
+        <h1 class="fs-1"><a id="scop" href="#scop" class="text-galben text-underline">#Calendar concurs:</a></h1>
+        <ol>
+            <li>Pregătirea şi publicarea concursului:<span class="fw-bold"> 01.11.2023-29.02.2024; </span></li>
+            <li>Faza locală/județeană<span class="fw-bold">01.02.2024-10.02.2024;</span></li>
+            <li>Înscrierea candidaţilor (vezi fişa de înscriere):<span class="fw-bold">12.02.2024-10.03.2023;</span></li>
+            <li>Primirea lucrărilor:<span class="fw-bold">12.02.2024-10.03.2024; *</span></li>
+            <li>Jurizarea lucrărilor:<span class="fw-bold">11.03.2024-13.03.2024;</span></li>
+            <li>Premierea: 	<span class="fw-bold">16.03.2024.</span></li>
+        </ol>
+        <p class="fs-6 fst-italic my-0">* lucrările plastice/literare expediate prin poștă vor fi primite până la data de 10.03.2024</p>
+        <h1 class="fs-1"><a id="obiective" href="#obiective" class="text-galben text-underline">#Secțiuni</a></h1>
+        <ol>
+            <li><span class="fw-bold">Secţiunea creaţie literară</span> gimnaziu(clasele a VII-a / a VIII-a),  / liceu (clasele a IX-a / a XII-a) <span class="fw-bold">(eseu)</span></li>
+            <li><span class="fw-bold">Secţiunea creaţie plastică</span> gimnaziu(clasele a VII-a / a VIII-a),   / liceu (clasele a IX-a / a XII-a)<span class="fw-bold"> (compoziţii plastice, afişe, postere)</span></li>
+            <li><span class="fw-bold">Secţiunea pentru cadre didactice</span> eseu liber – <span class="fw-bold">Cunoaştere şi autocunoaştere în aforismele lui Brâncuşi</span></li>
+        </ol>
+        <h1 class="fs-1"><a id="obiective" href="#obiective" class="text-galben text-underline">Condiții de participare</a></h1>
+        <span class="fs-1 text-albastru">C</span>oncursul este gratuit şi deschis tuturor elevilor din județ şi din țară. Aceştia vor putea participa la concurs cu o singură lucrare pe secţiune, iar un cadru didactic poate participa cu maximum 3 elevi.
+        <ol class="my-3">
+            <li>
+                <p><span class="fw-bold"> Secţiunea creaţie literară</span> se va desfăşura direct/ indirect . Fişele de înscriere (ANEXA 1), completate de profesorul coordonator şi lucrările se  trimit prin e-mail, la adresa: chilibandenisa@yahoo.com, în termenele precizate mai sus. Alături de fişele de înscriere şi de lucrări, doritorii pot trimite acordul de parteneriat semnat şi ştampilat.</p>
+                <p>Creaţiile literare se trimit și pe adresa şcolii: <span class="fw-bold"> Colegiul Naţional „Tudor Vladimirescu” Târgu Jiu, Str. Unirii, Nr. 13, Jud. Gorj </span>, cu menţiunea pentru <span class="fw-bold"> Simpozionul Național „Aforismele lui Bâncuși </span> – <span class="fw-bold"> izvor al exprimării artistice”. </span></p>
+                <p>Pentru elevii de gimnaziu şi liceu se va trimite  câte o creaţie, în proză (1-2 pagini) cu tema concursului. </p>
+                <p><span class="fw-bold">  ATENŢIE! </span> În materialele trimise trebuie menţionate: titlul lucrării, numele şi prenumele elevilor participanţi, clasa, numele şi specialitatea profesorilor coordonatori, şcoala de provenienţă şi judeţul. </li>
+                <p class="fs-6 fst-italic">Recomandări pentru tehnoredactare text: format A4, Microsoft Office Word, font Times New Roman, corp de literă 12, spaţiu între rânduri 1,5; titlul lucrării, numele, prenumele şi specialitatea cadrelor didactice participante, şcoala de provenienţă şi judeţul vor avea corp de literă 14, bold.</p>    
+            <li>
+                <p><span class="fw-bold">Secţiunea creaţie plastică </span>se va desfăşura direct/ indirect . Fişele de înscriere, completate de profesorul coordonator, se  trimit prin e-mail, la adresa: <span class="fw-bold"> gmititica@yahoo.com </span> în termenul precizat mai sus. Alături de fişele de înscriere şi de lucrări, doritorii pot trimite acordul de parteneriat semnat şi ştampilat.</p>
+                <p>Creaţiile plastice se trimit pe adresa şcolii: <span class="fw-bold"> Colegiul Naţional „Tudor Vladimirescu” Târgu Jiu, Str. Unirii, Nr. 13, Jud. Gorj</span>, cu menţiunea pentru <span class="fw-bold">Simpozionul Național „Aforismele lui Bâncuși</span> – <span class="fw-bold"> izvor al exprimării artistice”. </span>Lucrările vor fi realizate în tehnică la alegere (acuarelă, acrilic,guaşă, tempera, grafică), format A3, maxim 50/70. Lucrările trebuie să ilustreze unul dintre aforismele lui Constantin Brâncuși. Pe spatele lucrării, în dreapta jos, se aplică o etichetă pe care se va specifica lizibil, cu majuscule, titlul lucrării, numele şi prenumele elevului, clasa, unitatea şcolară, localitatea, judeţul, numele şi specialitatea profesorului coordonator. 
+   	Se va alege un aforism care va fi menţionat la începutul eseului, iar la creaţiile plastice în colţul din dreapta jos.</p>
+                <p>Se va alege un <span class="fw-bold"> aforism</span> care va fi menţionat la <span class="fw-bold"> începutul eseului</span>, iar la creaţiile plastice în <span class="fw-bold"> colţul din dreapta jos.</span></p>
+            </li>
+            <li>
+            <p><span class="fw-bold"> Secţiunea pentru cadre didactice </span> se va desfăşura on-line, la fel ca secţiunea creaţii literare. ANEXA 2 completată şi lucrările se  trimit prin e-mail, la adresa: chilibandenisa@yahoo.com, în termenele precizate mai sus. Alături de fişele de înscriere şi de lucrări, doritorii pot trimite acordul de parteneriat semnat şi ştampilat.</p>
+            <p class="fs-6 fst-italic">Recomandări pentru tehnoredactare text: format A4, Microsoft Office Word, font Times New Roman, corp de literă 12, spaţiu între rânduri 1,5; titlul lucrării, numele, prenumele şi specialitatea cadrelor didactice participante, şcoala de provenienţă şi judeţul vor avea corp de literă 14, bold.</p>
+            </li>
+        </ol>
+        <h1 class="fs-1"><a id="obiective" href="#obiective" class="text-galben text-underline">Jurizarea</a></h1>
+        <ul>
+            <li>Juriul va fi format din cadre didactice de specialitate din învăţământul preuniversitar. Se vor acorda premii în bani, trei premii (I, II şi III) şi o menţiune, pentru fiecare categorie de vârstă, respectiv 13-14 ani (clasele a VII-a / a VIII-a), 15-16 ani (clasele a IX-a / a X/a) și 17-18 ani (clasele a XI-a / a XII-a), pentru fiecare secţiune.</li>
+            <li>Diplome de participare se vor acorda pentru toţi elevii şi cadrele didactice îndrumătoare.</li>
+            <li>Diplome şi adeverinţe de participare la simpozion se vor acorda cadrelor didactice care au trimis lucrări la secţiunea pentru cadre didactice.</li>
+            <li>Diplomele se vor trimite prin poștă, în perioada 20.03.2024-20.04.2024, sau pe adresele de e-mail indicate de îndrumători în fişa de înscriere.</li>
+            <li>Nu se admit contestaţii. Lucrările elevilor nu se restituie.</li>
+        </ul>
+        <h1 class="fs-1"><a id="obiective" href="#obiective" class="text-galben text-underline">Evaluare</a></h1>
+        <ul>
+            <li>Expoziții</li>
+            <li>Portofolii</li>
+            <li>Afișe</li>
+            <li>Fotografii</li>
+            <li>Diplome</li>
+        </ul>
+        <h1 class="fs-1"><a id="obiective" href="#obiective" class="text-galben text-underline">Promovarea și mediatizarea concursului:</a></h1>
+        <ul>
+            <li>Anunț pe site-ul IȘJ Gorj;</li>
+            <li>Mediatizarea prin revista şcolii; Articole în presa locală;</li>
+            <li>Redactarea revistei simpozionului;</li>
+            <li>Realizarea unei PPT cu lucrările expuse şi premiate, pentru a putea fi apreciate calitativ şi de alţi elevi / cadre didactice / membri ai comunităţii;</li>
+            <li>Menţinerea expoziţiei pe o perioadă de 2 săptămâni, pentru a putea fi vizitată de alte instituţii de învăţământ sau alte persoane.</li>
+        </ul>
+        <h1 class="fs-1"><a id="obiective" href="#obiective" class="text-galben text-underline">Contact</a></h1>
+        <ul>
+            <li> Prof. Mititica Gabriela: gmititica@yahoo.com; tel.0761296133</li>
+            <li> Prof. Chiliban Denisa: chilibandenisa@yahoo.com; tel.0769263146</li>
+        </ul>
+    </div>
+    
     <!-- includem componenta de footer --->
     <?php include_once('static/pages/footer.php'); ?>
 
